335d581ed9e8929ee4ff20071d9430e2070d6985
Aivars <aivars.meijers@gmail.com>
2020-08-15 08:56:05 +0300

    DF Symbols + list row

---
+++ Shared/CoursesView.swift
@@ -0,0 +1,23 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Aivars Meijers on 15/08/2020.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        List(0 ..< 20) { item in
+            CourseRow()
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
