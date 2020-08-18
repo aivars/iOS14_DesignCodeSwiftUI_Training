f96c59aea2cc2d71677c20cbe7f90dc9dd3e66c5
Aivars <aivars.meijers@gmail.com>
2020-08-18 23:03:11 +0300

    Matched Geometry effect

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -9,17 +9,28 @@ import SwiftUI
 
 struct CoursesView: View {
     @State var show = false
+    @Namespace var namespace
     
     var body: some View {
         ZStack {
             CourseItem()
+                .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
                 .frame(width: 335, height: 250)
-            VStack {
-                if show {
+            if show {
+                ScrollView {
                     CourseItem()
-                        .transition(.move(edge: .leading))
-                        .edgesIgnoringSafeArea(.all)
+                        .matchedGeometryEffect(id: "Card", in: namespace)
+                        .frame( height: 300)
+                    VStack {
+                        ForEach(0 ..< 20) { item in
+                            CourseRow()
+                        }
+                    }
+                    .padding()
                 }
+                .transition(.opacity)
+                .edgesIgnoringSafeArea(.all)
+                
             }
         }
         .onTapGesture {
@@ -27,7 +38,7 @@ struct CoursesView: View {
                 show.toggle()
             }
         }
-//        .animation(.spring())
+        //        .animation(.spring())
     }
 }
 
