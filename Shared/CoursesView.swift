5af14bece0f3fdd3523bf5f75e54a9eb893f468e
Aivars <aivars.meijers@gmail.com>
2020-08-19 11:43:39 +0300

    Mach cards and static data

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct CoursesView: View {
     @State var show = false
     @Namespace var namespace
@@ -14,21 +16,21 @@ struct CoursesView: View {
     var body: some View {
         ZStack {
             ScrollView {
-                VStack (spacing: 20) {
-                    CourseItem()
-                        .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
-                        .frame(width: 335, height: 250)
-                    CourseItem()
-                        .frame(width: 335, height: 250)
+                VStack(spacing: 20) {
+                    ForEach(courses) { item in
+                        CourseItem(course: item)
+                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame(width: 335, height: 250)
+                    }
                 }
                 .frame(maxWidth: .infinity)
             }
             if show {
                 ScrollView {
-                    CourseItem()
-                        .matchedGeometryEffect(id: "Card", in: namespace)
-                        .frame( height: 300)
-                    VStack (spacing: 5) {
+                    CourseItem(course: courses[0])
+                        .matchedGeometryEffect(id: courses[0].id, in: namespace)
+                        .frame(height: 300)
+                    VStack {
                         ForEach(0 ..< 20) { item in
                             CourseRow()
                         }
@@ -37,13 +39,14 @@ struct CoursesView: View {
                 }
                 .background(Color("Background 1"))
                 .transition(
-                    .asymmetric(insertion: AnyTransition
+                    .asymmetric(
+                        insertion: AnyTransition
                                     .opacity
-                                    .animation(Animation.spring()
-                                                .delay(0.3)),
-                                removal: AnyTransition
-                                    .opacity
-                                    .animation(.spring()))
+                                    .animation(Animation.spring().delay(0.3)),
+                        removal: AnyTransition
+                            .opacity
+                            .animation(.spring())
+                    )
                 )
                 .edgesIgnoringSafeArea(.all)
             }
@@ -53,7 +56,6 @@ struct CoursesView: View {
                 show.toggle()
             }
         }
-        //        .animation(.spring())
     }
 }
 
