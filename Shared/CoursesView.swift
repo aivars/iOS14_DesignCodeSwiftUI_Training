daa8e96f2295e808a3932fba238906d388dcb503
Aivars <aivars.meijers@gmail.com>
2020-08-23 06:04:59 +0300

    18. Animating Above Navigation

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -13,12 +13,18 @@ struct CoursesView: View {
     @Namespace var namespace2
     @State var selectedItem: Course? = nil
     @State var isDisabled = false
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    #endif
     
     var body: some View {
         ZStack {
             #if os(iOS)
-            content
-                .navigationBarHidden(true)
+            if horizontalSizeClass == .compact {
+                tabBar
+            } else {
+                sideBar
+            }
             fullContent
                 .background(VisualEffectBlur(blurStyle: .systemMaterial).edgesIgnoringSafeArea(.all))
             #else
@@ -33,13 +39,6 @@ struct CoursesView: View {
     var content: some View {
         ScrollView {
             VStack(spacing: 0) {
-                Text("Courses")
-                    .font(.largeTitle)
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 16)
-                    .padding(.top, 54)
-                
                 LazyVGrid(
                     columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
                     spacing: 16
@@ -84,6 +83,7 @@ struct CoursesView: View {
             }
         }
         .zIndex(1)
+        .navigationTitle("Courses")
     }
     
     @ViewBuilder
@@ -108,6 +108,76 @@ struct CoursesView: View {
             .frame(maxWidth: 712)
             .frame(maxWidth: .infinity)
         }
+    }
+    
+    var tabBar: some View {
+        TabView {
+            NavigationView {
+                content
+            }
+            .tabItem {
+                Image(systemName: "book.closed")
+                Text("Courses")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "list.bullet.rectangle")
+                Text("Tutorials")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "tv")
+                Text("Livestreams")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "mail.stack")
+                Text("Certificates")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("Search")
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var sideBar: some View {
+        #if os(iOS)
+        NavigationView {
+            List {
+                NavigationLink(destination: CoursesView()) {
+                    Label("Courses", systemImage: "book.closed")
+                }
+                Label("Courses", systemImage: "book.closed")
+                Label("Tutorials", systemImage: "list.bullet.rectangle")
+                Label("Livestreams", systemImage: "tv")
+                Label("Certificates", systemImage: "mail.stack")
+                Label("Search", systemImage: "magnifyingglass")
+            }
+            .listStyle(SidebarListStyle())
+            .navigationTitle("Learn")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "person.crop.circle")
+                }
+            }
+            content
+        }
+        #endif
     }
 }
 
